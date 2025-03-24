c7c1897b6873c702fe98cc622019bfdfc58a20ed
Alex&Indie <92387131+AlexIndiex@users.noreply.github.com>
2025-03-24 22:05:49 +0100

    Update updater-v2.sh

--- updater-v2.sh
+++ updater-v2.sh
@@ -32,14 +32,14 @@ download_notify() {
     
     case $app_name in
         Citron)
-            mapfile -t urls < <(curl -s -H "https://git.citron-emu.org/api/v1/repos" -G -d 'per_page=1' https://git.citron-emu.org/api/v1/repos/Citron/Citron/releases | \
-                jq -r '.[].assets[] | select(.browser_download_url | test("AppImage")) | .browser_download_url')
+            mapfile -t urls < <(curl -s -H "Accept: application/vnd.github+json" -G -d 'per_page=1' https://api.github.com/repos/Samueru-sama/Citron-AppImage-test/releases | \
+                    jq -r '[.[] | select(.tag_name == "nightly")][].assets[] | select(.browser_download_url | test("anylinux-x86_64_v3.AppImage")) | .browser_download_url')
             file_name="Citron.AppImage"
             ;;
         Ryujinx)
-            mapfile -t url < <(curl -s -H "Accept: application/vnd.github+json" -G -d 'per_page=1' https://api.github.com/repos/GreemDev/Ryujinx-Canary/releases | \
-                    jq -r '.[].assets[] | select(.browser_download_url | test("linux_x64")) | .browser_download_url')
-            file_name="Ryujinx.tar.gz"
+            mapfile -t url < <(curl -s -H "Accept: application/vnd.github+json" -G -d 'per_page=1' https://api.github.com/repos/Ryubing/Canary-Releases/releases | \
+                    jq -r '.[].assets[] | select(.browser_download_url | test("x64.AppImage")) | .browser_download_url')
+            file_name="Ryujinx.AppImage"
             ;;
         Cemu)
             mapfile -t url < <(curl -s -H "Accept: application/vnd.github+json" -G -d 'per_page=1' https://api.github.com/repos/cemu-project/Cemu/releases | \
@@ -78,15 +78,15 @@ download_notify() {
             url="https://nightly.link/mandarine3ds/mandarine/workflows/build/master/linux-appimage.zip"
             file_name="mandarine.zip"
             ;;
-        Lime3DS)
-            mapfile -t url < <(curl -s -H "Accept: application/vnd.github+json" -G -d 'per_page=1' https://api.github.com/repos/Lime3DS/Lime3DS/releases | \
+        azahar)
+            mapfile -t url < <(curl -s -H "Accept: application/vnd.github+json" -G -d 'per_page=1' https://api.github.com/repos/azahar-emu/azahar/releases | \
                     jq -r '.[].assets[] | select(.browser_download_url | test("appimage")) | .browser_download_url')
-            file_name="Lime3DS.tar.gz"
+            file_name="azahar.tar.gz"
             ;;
-        GearBoy)
+        gearboy)
             mapfile -t url < <(curl -s -H "Accept: application/vnd.github+json" -G -d 'per_page=1' https://api.github.com/repos/drhelius/Gearboy/releases | \
                     jq -r '.[].assets[] | select(.browser_download_url | test("ubuntu")) | .browser_download_url')
-            file_name="Gearboy.zip"
+            file_name="gearboy.zip"
             ;;
         bsnes)
             mapfile -t url < <(curl -s -H "Accept: application/vnd.github+json" -G -d 'per_page=1' https://api.github.com/repos/bsnes-emu/bsnes/releases | \
@@ -123,12 +123,7 @@ download_notify() {
             notify "Update successful: $app_name"
             # Extract and set permissions
             case $app_name in
-                Ryujinx)
-                    tar xf "$HOME/Apps/$file_name" -C "$HOME/Apps/"
-                    chmod +x "$HOME/Apps/publish/Ryujinx" "$HOME/Apps/publish/Ryujinx.sh" "$HOME/Apps/publish/Ryujinx.SDL2.Common.dll.config" "$HOME/Apps/publish/mime/Ryujinx.xml"
-                    xdg-open https://free-git.org/Emulator-Archive/torzu/releases
-                    ;;
-                Cemu | DolphinDev | RMG | mGBAdev | snes9x | Citron)
+                Cemu | DolphinDev | RMG | mGBAdev | snes9x | Citron | Ryujinx)
                     chmod +x "$HOME/Apps/$file_name"
                     ;;
                 Panda3DS | melonDS | SkyEmu)
@@ -136,22 +131,22 @@ download_notify() {
                     mv -f "$HOME/Apps/Alber-x86_64.AppImage" "$HOME/Apps/Panda3DS.AppImage" && chmod +x "$HOME/Apps/Panda3DS.AppImage"
                     chmod +x "$HOME/Apps/$app_name"
                     ;;
-                GearBoy | bsnes | sudachi)
+                gearboy | bsnes | sudachi)
                     7z x "$HOME/Apps/$file_name" -o* -y
-                    chmod +x "$HOME/Apps/$app_name/sudachi" "$HOME/Apps/$app_name/sudachi-cmd" "$HOME/Apps/$app_name/tzdb2nx" "$HOME/Apps/$app_name/gearboy" "$HOME/Apps/$app_name/bsnes-nightly/bsnes"
+                    chmod +x "$HOME/Apps/$app_name/$app_name" "$HOME/Apps/$app_name/$app_name-cmd" "$HOME/Apps/$app_name/tzdb2nx" "$HOME/Apps/$app_name/bsnes-nightly/$app_name"
                     ;;
-                Lime3DS)
-                    [ -d "$HOME/Apps/Lime3DS" ] || mkdir -p "$HOME/Apps/Lime3DS"
-                    tar xf "$HOME/Apps/$file_name" -C "$HOME/Apps/Lime3DS" --strip-components=1
-                    chmod +x "$HOME/Apps/$app_name/lime3ds-cli.AppImage" "$HOME/Apps/$app_name/lime3ds-gui.AppImage" "$HOME/Apps/$app_name/lime3ds-room.AppImage"
+                azahar)
+                    [ -d "$HOME/Apps/$app_name" ] || mkdir -p "$HOME/Apps/$app_name"
+                    tar xf "$HOME/Apps/$file_name" -C "$HOME/Apps/Azahar" --strip-components=1
+                    chmod +x "$HOME/Apps/$app_name/$app_name.AppImage" "$HOME/Apps/$app_name/$app_name-room.AppImage"
                     ;;
                 mandarine)
-                    [ -d "$HOME/Apps/mandarine" ] || mkdir -p "$HOME/Apps/mandarine"
+                    [ -d "$HOME/Apps/$app_name" ] || mkdir -p "$HOME/Apps/$app_name"
                     7z x "$HOME/Apps/$file_name" -y
-                    mv -f "$HOME/Apps/mandarine-linux-appimage-*.tar.gz" "$HOME/Apps/mandarine.tar.gz"
-                    tar xf "$HOME/Apps/mandarine.tar.gz" -C "$HOME/Apps/mandarine" --strip-components=1 
-                    rm -f "$HOME/Apps/mandarine.tar.gz"
-                    chmod +x "$HOME/Apps/mandarine/mandarine-cli.AppImage" "$HOME/Apps/mandarine/mandarine-gui.AppImage" "$HOME/Apps/mandarine/mandarine-room.AppImage"
+                    mv -f "$HOME/Apps/$app_name-linux-appimage-*.tar.gz" "$HOME/Apps/$app_name.tar.gz"
+                    tar xf "$HOME/Apps/$app_name.tar.gz" -C "$HOME/Apps/$app_name" --strip-components=1 
+                    rm -f "$HOME/Apps/$app_name.tar.gz"
+                    chmod +x "$HOME/Apps/mandarine/$app_name.AppImage" "$HOME/Apps/mandarine/$app_name-qt.AppImage" "$HOME/Apps/mandarine/$app_name-room.AppImage"
                     ;;
                 *)
                     ;;
@@ -181,8 +176,8 @@ download_notify RMG
 download_notify melonDS
 download_notify SkyEmu
 download_notify mGBAdev
-download_notify Lime3DS
-download_notify GearBoy 
+download_notify azahar
+download_notify gearboy 
 download_notify bsnes 
 download_notify snes9x
 download_notify mandarine
